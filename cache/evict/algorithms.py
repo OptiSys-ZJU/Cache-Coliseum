2e9b773b50a046871367ed4283c5a54b2fac3028
freesia <freesia>
2024-12-21 16:34:54 +0800

    modified:   benchmark/__main__.py 	modified:   cache/cache.py 	new file:   cache/evict/__init__.py 	renamed:    cache/evict_algorithms.py -> cache/evict/algorithms.py 	new file:   cache/evict/evictor.py 	new file:   cache/evict/predictor.py

--- cache/evict/algorithms.py
+++ cache/evict/algorithms.py
@@ -1,49 +1,12 @@
 from abc import ABC, abstractmethod
 from functools import partial
-import collections
-from typing import Type, List, Union
+from typing import List, Union
+from cache.evict.evictor import *
+from cache.evict.predictor import *
+import numpy as np
 import types
 import copy
-
 import random
-import numpy as np
-
-class Evictor(ABC):
-    @abstractmethod
-    def evict(self, candidates):
-        pass
-
-class ReuseDistanceEvictor(Evictor):
-    def evict(self, candidates):
-        return max(candidates, key=lambda x: x[1])[0]
-
-class BinaryEvictor(Evictor):
-    def evict(self, candidates):
-        indices_with_1 = [i for i, pred in candidates if pred == 1]
-        if indices_with_1:
-            chosen_index = random.choice(indices_with_1)
-        else:
-            chosen_index = random.choice(candidates)[0]
-        return chosen_index
-
-class LRUEvictor(Evictor):
-    def evict(self, candidates):
-        return min(candidates, key=lambda x: x[1])[0]
-
-class MarkerEvictor(Evictor):
-    def evict(self, candidates):
-        # 1 marked, 0 unmarked
-        indices_with_0 = [i for i, mark in candidates if mark == 0]
-        if indices_with_0:
-            chosen_index = random.choice(indices_with_0)
-        else:
-            raise ValueError('MarkerEvictor: all marked')
-
-        return chosen_index
-
-class RandEvictor(Evictor):
-    def evict(self, candidates):
-        return random.choice([i for i, _ in candidates])
 
 class EvictAlgorithm(ABC):
     """Evict an entry from one cache line
@@ -57,87 +20,6 @@ class EvictAlgorithm(ABC):
     @abstractmethod
     def access(self, key) -> bool:
         pass
-
-class Predictor(ABC):
-    @abstractmethod
-    def pred(self, key, ts):
-        pass
-
-class ReuseDistancePredictor(Predictor):
-    def pred(self, key, ts):
-        pass 
-
-class BinaryPredictor(Predictor):
-    def pred(self, key, ts):
-        pass 
-
-class OraclePredictor(ABC):
-    def __init__(self, reuse_dis_noise_sigma=0) -> None:
-        self.reuse_dis_noise_sigma = reuse_dis_noise_sigma
-    
-    def oracle_access(self, key, next_access_time):
-        if self.reuse_dis_noise_sigma == 0:
-            self.__oracle_access__(key, next_access_time)
-        else:
-            self.__oracle_access__(key, np.random.normal(next_access_time, self.reuse_dis_noise_sigma))
-
-    @abstractmethod
-    def __oracle_access__(self, key, next_access_time):
-        pass
-
-class OracleReuseDistancePredictor(ReuseDistancePredictor, OraclePredictor):
-    def __init__(self, reuse_dis_noise_sigma=0, oracle_check=True) -> None:
-        super().__init__(reuse_dis_noise_sigma)
-        self.oracle_preds = collections.deque()
-        self.oracle_check = oracle_check
-    
-    def pred(self, key, ts):
-        oracle_key, next_access_time = self.oracle_preds.popleft()
-        if self.oracle_check and oracle_key != key:
-            raise ValueError("OracleReuseDistancePredictAlgorithm: oracle key not equals to key")
-        return next_access_time
-
-    def __oracle_access__(self, key, next_access_time):
-        self.oracle_preds.append((key, next_access_time))
-
-class OracleBinaryPredictor(BinaryPredictor, OraclePredictor):
-    def __init__(self, associativity, reuse_dis_noise_sigma=0, bin_noise_prob=0, oracle_check=True) -> None:
-        super().__init__(reuse_dis_noise_sigma)
-        self.oracle_cache = [None] * associativity
-        self.oracle_preds = collections.deque()
-        self.oracle_check = oracle_check
-        self.oracle_next_access_times = [np.inf] * associativity
-        self.oracle_last_time = {}
-        self.oracle_t = 0
-        self.bin_noise_prob = bin_noise_prob
-    
-    def pred(self, key, ts):
-        oracle_key, bin_pred = self.oracle_preds.popleft()
-        if self.oracle_check and oracle_key != key:
-            raise ValueError("OracleBinaryPredictAlgorithm: oracle key not equals to key")
-
-        if self.bin_noise_prob == 0:
-            return bin_pred
-        else:
-            if random.random() < self.bin_noise_prob:
-                return 1 - bin_pred
-            else:
-                return bin_pred
-
-    def __oracle_access__(self, key, next_access_time):
-        if key in self.oracle_cache:
-            target_index = self.oracle_cache.index(key)
-        elif None in self.oracle_cache:
-            target_index = self.oracle_cache.index(None)
-        else:
-            target_index = self.oracle_next_access_times.index(max(self.oracle_next_access_times))
-            self.oracle_preds[self.oracle_last_time[self.oracle_cache[target_index]]][1] = 1
-        
-        self.oracle_cache[target_index] = key
-        self.oracle_next_access_times[target_index] = next_access_time 
-        self.oracle_preds.append([key, 0])
-        self.oracle_last_time[key] = self.oracle_t
-        self.oracle_t += 1
 
 class OracleAlgorithm():
     def oracle_access(self, key, next_access_time):
@@ -176,11 +58,6 @@ class PredictAlgorithm(EvictAlgorithm):
         self.cache[target_index] = key
         self.preds[target_index] = self.trigger_pred(key)
         return hit
-
-class ReuseDistancePredition:
-    pass
-class BinaryPredition:
-    pass
 
 class GuardAlgorithm(PredictAlgorithm):
     def __init__(self, associativity, evictor: Evictor, predictor: Predictor, relax_times=0, relax_prob=0) -> None:
