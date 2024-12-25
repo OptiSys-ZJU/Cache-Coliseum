163cfd81106abf3a4d2fc2e76ac0e3cf5882db9e
freesia <freesia>
2024-12-25 17:36:17 +0800

    modified:   .gitignore 	modified:   benchmark/__main__.py 	modified:   cache/evict/algorithms.py 	modified:   cache/evict/predictor.py

--- cache/evict/predictor.py
+++ cache/evict/predictor.py
@@ -15,13 +15,16 @@ class Predictor(ABC):
 
 class ReuseDistancePredictor(Predictor):
     pass
-
 class BinaryPredictor(Predictor):
+    pass
+class PhasePredictor(Predictor):
     pass
 
 class ReuseDistancePredition:
     pass
 class BinaryPredition:
+    pass
+class PhasePredition:
     pass
 
 class OraclePredictor(ABC):
@@ -32,7 +35,7 @@ class OraclePredictor(ABC):
         if self.reuse_dis_noise_sigma == 0:
             self.__oracle_access__(pc, address, next_access_time)
         else:
-            self.__oracle_access__(pc, address, np.random.normal(next_access_time, self.reuse_dis_noise_sigma))
+            self.__oracle_access__(pc, address, next_access_time + np.random.lognormal(0, self.reuse_dis_noise_sigma))
 
     @abstractmethod
     def __oracle_access__(self, pc, address, next_access_time):
@@ -92,6 +95,46 @@ class OracleBinaryPredictor(BinaryPredictor, OraclePredictor):
         self.oracle_last_time[address] = self.oracle_t
         self.oracle_t += 1
 
+class OraclePhasePredictor(PhasePredictor, OraclePredictor):
+    def __init__(self, associativity, reuse_dis_noise_sigma=0, bin_noise_prob=0, oracle_check=True):
+        super().__init__(reuse_dis_noise_sigma)
+        self.associativity = associativity
+
+        self.oracle_t = 0
+        self.oracle_preds = collections.deque()
+        self.oracle_check = oracle_check
+        self.bin_noise_prob = bin_noise_prob
+
+        self.oracle_prev_phase = []
+        self.oracle_curr_phase = []
+        self.oracle_curr_key_set = set()
+    
+    def pred_after_evict(self, ts, pc, address):
+        oracle_key, bin_pred = self.oracle_preds.popleft()
+        if self.oracle_check and oracle_key != address:
+            raise ValueError("OraclePhasePredictorm: oracle key not equals to key")
+
+        if self.bin_noise_prob == 0:
+            return bin_pred
+        else:
+            if random.random() < self.bin_noise_prob:
+                return 1 - bin_pred
+            else:
+                return bin_pred
+
+    def __oracle_access__(self, pc, address, next_access_time):
+        if len(self.oracle_curr_key_set) == self.associativity:
+            for key, t in self.oracle_prev_phase:
+                if key in self.oracle_curr_key_set:
+                    self.oracle_preds[t][1] = 0
+            self.oracle_prev_phase = self.oracle_curr_phase
+            self.oracle_curr_phase = []
+            self.oracle_curr_key_set = set()
+        self.oracle_curr_phase.append((address, self.oracle_t))
+        self.oracle_curr_key_set.add(address)
+        self.oracle_preds.append([address, 1])
+        self.oracle_t += 1
+        
 class ParrotPredictor(Predictor):
     def __init__(self, shared_model):
         self._model = shared_model
