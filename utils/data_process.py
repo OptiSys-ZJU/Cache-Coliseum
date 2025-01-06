64928be162bd7c0bccdc81b2ba69f15fd4e18b36
freesia <freesia>
2025-01-06 21:56:21 +0800

    modified:   .gitignore 	modified:   scripts/run_bin.sh 	modified:   scripts/run_dis.sh 	modified:   scripts/run_logdis.sh 	modified:   utils/data_process.py

--- utils/data_process.py
+++ utils/data_process.py
@@ -1,9 +1,45 @@
 import pandas as pd
 import os
+import csv
 
 if __name__ == '__main__':
     name_dict = {
         'LRU': 'LRU',
+
+        'OracleLogDis': {
+            'Predict[Belady]': 'ftp',
+            'PredMark[Belady]': 'predmarker',
+            'LMarker[Belady]': 'lmarker',
+            'LNonMarker[Belady]': 'lnonmarker',
+            'FollowerRobust[OracleState]': "fr",
+            'Guard[Belady]-f-pred-no-relax': 'guardftp0',
+            'Guard[Belady]-f-pred-relax-times-5': 'guardftp5',
+            'CombDet[Predict[Belady], Marker]': 'blindoracledftp',
+            'CombineRandom[Predict[Belady], Marker]': 'blindoraclerftp',
+        },
+
+        'OracleDis': {
+            'Predict[Belady]': 'ftp',
+            'PredMark[Belady]': 'predmarker',
+            'LMarker[Belady]': 'lmarker',
+            'LNonMarker[Belady]': 'lnonmarker',
+            'FollowerRobust[OracleState]': "fr",
+            'Guard[Belady]-f-pred-no-relax': 'guardftp0',
+            'Guard[Belady]-f-pred-relax-times-5': 'guardftp5',
+            'CombDet[Predict[Belady], Marker]': 'blindoracledftp',
+            'CombineRandom[Predict[Belady], Marker]': 'blindoraclerftp',
+        },
+
+        'OracleBin': {
+            'Predict[FBP]': 'fbp',
+            'Mark0[FBP]': 'mark0',
+            'Mark&Predict[OraclePhase]': 'markpred',
+            'CombDet[Predict[FBP], Marker]': 'blindoracledfbp',
+            'CombineRandom[Predict[FBP], Marker]': 'blindoraclerfbp',
+            'Guard[FBP]-f-pred-no-relax': 'guardfbp0',
+            'Guard[FBP]-f-pred-relax-times-5': 'guardfbp5',
+        },
+
         'PLECO': {
             'Marker': 'Marker',
             'Predict[PLECO]': 'FTP',
@@ -51,13 +87,14 @@ if __name__ == '__main__':
         },
 
     }
-    mode = 'GBMBin'
+    mode = 'OracleDis'
     root_dir_path = 'stat'
+    res_csv_path = 'plot_res'
     res_dict = {}
     for dirpath in os.listdir(root_dir_path):
         dataset = dirpath
-        if dataset == 'brightkite' or dataset == 'citi':
-            continue
+        # if dataset == 'brightkite' or dataset == 'citi':
+        #     continue
         full_path = os.path.join(root_dir_path, dirpath)
         if os.path.isdir(full_path):
             if mode == 'GBMBin':
@@ -79,7 +116,81 @@ if __name__ == '__main__':
                                 if res_name not in res_dict[dataset]:
                                     res_dict[dataset][res_name] = []
                                 res_dict[dataset][res_name].append((frac, (cr-1)/(lru-1)))
-
+            elif mode == 'OracleLogDis':
+                this_dataset_plot_path = os.path.join(res_csv_path, dataset, 'logdis')
+                if not os.path.exists(this_dataset_plot_path):
+                    os.makedirs(this_dataset_plot_path)
+                dir_path = os.path.join(full_path, '1')                
+                this_path = os.path.join(dir_path, 'logdis.csv')
+                if os.path.exists(this_path):
+                    df = pd.read_csv(this_path)
+                    result_dict = df.set_index('Name').T.to_dict('dict')
+                    for name in result_dict:
+                        if name in name_dict[mode]:
+                            alg_name = name_dict[mode][name]
+                            res_path = os.path.join(this_dataset_plot_path, f'{alg_name}.csv')
+                            tuple_list = []
+                            for noise, value in result_dict[name].items():
+                                if noise.startswith('LogDis-'):
+                                    this_x = noise.split('-')[1]
+                                    this_y = value.split('/')[1]
+                                    tuple_list.append((this_x, this_y))
+                            with open(res_path, mode='w', newline='') as file:
+                                writer = csv.writer(file)
+                                writer.writerow(['x', 'y'])
+                                for tup in tuple_list:
+                                    writer.writerow(tup)
+                            print(f"Data has been written to {res_path}")
+            elif mode == 'OracleDis':
+                this_dataset_plot_path = os.path.join(res_csv_path, dataset, 'dis')
+                if not os.path.exists(this_dataset_plot_path):
+                    os.makedirs(this_dataset_plot_path)
+                dir_path = os.path.join(full_path, '1')                
+                this_path = os.path.join(dir_path, 'dis.csv')
+                if os.path.exists(this_path):
+                    df = pd.read_csv(this_path)
+                    result_dict = df.set_index('Name').T.to_dict('dict')
+                    for name in result_dict:
+                        if name in name_dict[mode]:
+                            alg_name = name_dict[mode][name]
+                            res_path = os.path.join(this_dataset_plot_path, f'{alg_name}.csv')
+                            tuple_list = []
+                            for noise, value in result_dict[name].items():
+                                if noise.startswith('Dis-'):
+                                    this_x = noise.split('-')[1]
+                                    this_y = value.split('/')[1]
+                                    tuple_list.append((this_x, this_y))
+                            with open(res_path, mode='w', newline='') as file:
+                                writer = csv.writer(file)
+                                writer.writerow(['x', 'y'])
+                                for tup in tuple_list:
+                                    writer.writerow(tup)
+                            print(f"Data has been written to {res_path}")
+            elif mode == 'OracleBin':
+                this_dataset_plot_path = os.path.join(res_csv_path, dataset, 'bin')
+                if not os.path.exists(this_dataset_plot_path):
+                    os.makedirs(this_dataset_plot_path)
+                dir_path = os.path.join(full_path, '1')                
+                this_path = os.path.join(dir_path, 'bin.csv')
+                if os.path.exists(this_path):
+                    df = pd.read_csv(this_path)
+                    result_dict = df.set_index('Name').T.to_dict('dict')
+                    for name in result_dict:
+                        if name in name_dict[mode]:
+                            alg_name = name_dict[mode][name]
+                            res_path = os.path.join(this_dataset_plot_path, f'{alg_name}.csv')
+                            tuple_list = []
+                            for noise, value in result_dict[name].items():
+                                if noise.startswith('Bin-'):
+                                    this_x = noise.split('-')[1]
+                                    this_y = value.split('/')[1]
+                                    tuple_list.append((this_x, this_y))
+                            with open(res_path, mode='w', newline='') as file:
+                                writer = csv.writer(file)
+                                writer.writerow(['x', 'y'])
+                                for tup in tuple_list:
+                                    writer.writerow(tup)
+                            print(f"Data has been written to {res_path}")
             else:
                 dir_path = os.path.join(full_path, '1')                
                 this_path = os.path.join(dir_path, 'pleco_popu_pleco-bin.csv')
